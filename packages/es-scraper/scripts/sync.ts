1153f47a1ec692068f586b4763c3cb14b5671680
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-07-08 01:08:49 -0700

    Simplify code and improve logging

--- packages/es-scraper/scripts/sync.ts
+++ packages/es-scraper/scripts/sync.ts
@@ -1,20 +1,23 @@
 import FS from "node:fs/promises";
+import path from "node:path";
 import { generatedPath } from "../src/utils.js";
 
-let oldSHA = null;
-
-try {
-  const revision = await FS.readFile(generatedPath("spec.html"), "utf-8");
-  oldSHA = revision.match(/<!-- REVISION: (?<sha>.*) -->/)!.groups!.sha!;
-} catch (e) {
-  // If we couldn't read the old file, continue
-}
 const { sha: newSHA } = await fetch(
   "https://api.github.com/repos/tc39/ecma262/commits/main",
 ).then((res) => res.json());
-if (oldSHA === newSHA) {
-  console.log("No new changes were found. Exiting.");
-  process.exit(0);
+
+try {
+  const revision = await FS.readFile(generatedPath("spec.html"), "utf-8");
+  const oldSHA = revision.match(/<!-- REVISION: (?<sha>.*) -->/)!.groups!.sha!;
+  if (oldSHA === newSHA) {
+    console.log("No new changes were found. Exiting.");
+    process.exit(0);
+  } else {
+    console.log("New version detected, downloading...");
+  }
+} catch (e) {
+  // If we couldn't read the old file, continue
+  console.log("No existing spec.html detected, downloading...");
 }
 
 // Cannot use the API endpoint because the file is too big
@@ -22,14 +25,15 @@ const data = await fetch(
   "https://raw.githubusercontent.com/tc39/ecma262/main/spec.html",
 ).then((res) => res.text());
 
+const specFilePath = generatedPath("spec.html");
+
 try {
-  await FS.access(generatedPath(""));
+  await FS.access(path.dirname(specFilePath));
 } catch (e) {
   // If the folder does not exist, create it
-  await FS.mkdir(generatedPath(""));
+  await FS.mkdir(path.dirname(specFilePath));
 }
 
-await FS.writeFile(
-  generatedPath("spec.html"),
-  `<!-- REVISION: ${newSHA} -->\n${data}`,
-);
+await FS.writeFile(specFilePath, `<!-- REVISION: ${newSHA} -->\n${data}`);
+
+console.log(`Download complete! Saved to ${specFilePath}.`);
