24ed1fa63ff3832c965929a2439ee6017d45e9e3
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-07-08 04:45:43 -0700

    Various fixes (#1)
    * Add ts-node as dev dependency to es-scraper package
    
    * Ignore failed attempt at reading old scraped file
    
    This is especially helpful in the event that the file doesn't exist.
    
    * Ensure that the generated/ folder exists before accessing spec.html file
    
    * Throw custom message if spec.html doesn't exist when running scrape
    
    * Update log message
    
    * Remove redundant log message
    
    * Simplify code and improve logging
    
    * Apply suggestions from code review
    
    * Update packages/es-scraper/scripts/sync.ts
    
    * More fixes
    
    * Ahem
    
    * Update packages/es-scraper/scripts/sync.ts
    
    ---------
    
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- packages/es-scraper/scripts/sync.ts
+++ packages/es-scraper/scripts/sync.ts
@@ -1,14 +1,32 @@
 import FS from "node:fs/promises";
+import Path from "node:path";
 import { generatedPath } from "../src/utils.js";
 
-const revision = await FS.readFile(generatedPath("spec.html"), "utf-8");
-const oldSHA = revision.match(/<!-- REVISION: (?<sha>.*) -->/)!.groups!.sha!;
+const specFilePath = generatedPath("spec.html");
+
+// Ensure the generated directory exists
+try {
+  await FS.access(Path.dirname(specFilePath));
+} catch {
+  await FS.mkdir(Path.dirname(specFilePath));
+}
+
 const { sha: newSHA } = await fetch(
   "https://api.github.com/repos/tc39/ecma262/commits/main",
 ).then((res) => res.json());
-if (oldSHA === newSHA) {
-  console.log("No new changes");
-  process.exit(0);
+
+try {
+  const revision = await FS.readFile(specFilePath, "utf-8");
+  const oldSHA = revision.match(/<!-- REVISION: (?<sha>.*) -->/)!.groups!.sha!;
+  if (oldSHA === newSHA) {
+    console.log("No new changes found. Exiting.");
+    process.exit(0);
+  } else {
+    console.log("New version detected. Downloading...");
+  }
+} catch {
+  // If we couldn't read the old file, continue
+  console.log("No existing spec.html detected. Downloading...");
 }
 
 // Cannot use the API endpoint because the file is too big
@@ -16,7 +34,6 @@ const data = await fetch(
   "https://raw.githubusercontent.com/tc39/ecma262/main/spec.html",
 ).then((res) => res.text());
 
-await FS.writeFile(
-  generatedPath("spec.html"),
-  `<!-- REVISION: ${newSHA} -->\n${data}`,
-);
+await FS.writeFile(specFilePath, `<!-- REVISION: ${newSHA} -->\n${data}`);
+
+console.log(`Download completed! Saved to ${specFilePath}.`);
